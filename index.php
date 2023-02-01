6ea5e5173b59864a07a2bebea629286f2be478e7
tufik <tufikhasan05@gmail.com>
2023-02-01 13:13:45 +0600

    define() -> Constants are immutable:

--- index.php
+++ index.php
@@ -1,32 +1,59 @@
 <?php
 /*
-PHP variable mutable:
-In PHP, a variable is considered mutable if its value can be changed or altered after it has been initially assigned. This means that the value stored in the variable can be modified or reassigned at any point during the execution of a program. For example, the value of a mutable variable can be changed through assignment statements, increment/decrement operations, or by being passed as an argument to a function that modifies its value.
+PHP Constants are immutable:
+In PHP, constants are considered immutable, which means that their values cannot be changed once they have been defined. Once a constant is defined using the define() function or the const keyword, its value remains the same throughout the execution of the program. Attempting to change the value of a constant will result in a runtime error.
+
+Constants are typically used to store values that are used throughout a program, such as configuration settings, commonly used values, or other values that are meant to remain constant. By using constants instead of variables, you can ensure that their values remain unchanged, providing a more predictable and secure environment for your code.
+
+######### For example: ##########
+define("MY_CONSTANT", "Hello, world!");
+echo MY_CONSTANT; // outputs "Hello, world!"
+
+// Attempting to change the value of a constant will result in an error:
+// MY_CONSTANT = "Goodbye, world!"; // Causes a fatal error
+
+
+********** To create a constant, use the define() ************
+Syntax: define(name, value, case-insensitive)  //Argument #3 is ignored since declaration of case-insensitive constants is no longer supported as of PHP 8.0.0
+
+Parameters:
+-> name: Specifies the name of the constant
+-> value: Specifies the value of the constant
+-> case-insensitive: Defining case-insensitive constants is deprecated as of PHP 7.3.0. As of PHP 8.0.0, only false is an acceptable value, passing true will produce a warning.
 */
-$firstName = "Tufik";
-echo $firstName;
+
+//Argument #3 is ignored since declaration of case-insensitive constants is no longer supported as of PHP 8.0.0
+// define("NAME","Tufik Hasan",true);
+
+define("NAME","Tufik Hasan");
+echo NAME;
 echo "\n";
-$firstName = "Towfik hasan";
-echo $firstName,"\n";
+
+define("PI",3.14159);
+//show constant value
+echo PI;  //output = 3.14159
+echo "\n";
 
 
+//show constant value
+echo constant("PI");  //output = 3.14159
+echo "\n";
 
-$a = 5;  // initial assignment
-$a = 10; // reassigning the value of $a
-echo $a,"\n";
 
-$b = 7;
-$b += 3; // changing the value of $b by incrementing it
-echo $b,"\n";
+//constants cannot be directly embedded in string literals
+echo "The PI value is PI ";  //output = The PI value is PI
+echo "\n";
 
-$c = 20;
-$c--;    // changing the value of $c by decrementing it
-echo $c,"\n";
+//constants cannot be directly embedded in string literals
+echo "The PI value is {PI}";  //output = The PI value is {PI}
+echo "\n";
 
-$d = 15;
-$d = $d + 5; // changing the value of $d through an expression
-echo $d,"\n";
+//Using concatenation:
+echo "The PI value is: ".PI;  //output = The PI value is {PI}: 3.14159
+echo "\n";
 
-$e = "hello";
-$e .= " world"; // changing the value of $e by concatenation
-echo $e,"\n";
+
+//tricks to show define constant value in string
+$constant = 'constant';
+echo "The PI value is {$constant("PI")}\n";   //output = The PI value is 3.14159
+echo "The PI value is {$constant("PI")}";   //output = The PI value is 3.14159
