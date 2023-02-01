a912fa9d94098e980f2a1e5e9741ffff0c6d65b0
tufik <tufikhasan05@gmail.com>
2023-02-01 10:05:22 +0600

    PHP variables

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/*
+PHP variables:
+A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume).
+Rules for PHP variables:
+    
+-> A variable starts with the $ sign, followed by the name of the variable
+-> A variable name must start with a letter or the underscore character
+-> A variable name cannot start with a number
+-> A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+-> Variable names are case-sensitive ($age and $AGE are two different variables)
+*/
+
+//variable define
+$name = "Towfik";
+//camelCase variable
+$firstName = "Nadim";
+//TitleCase variable
+$LastName = "Hassan";
+//variable with underscore
+$full_name = "Rakib Khan";
