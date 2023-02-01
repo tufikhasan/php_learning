b5b78ea3b65987a3f2a202cd5dd24d0ce1938582
tufik <tufikhasan05@gmail.com>
2023-02-01 11:25:35 +0600

    Variables Scope

--- index.php
+++ index.php
@@ -1,66 +1,87 @@
 <?php
 /*
-What does $$ ($$variable) means in PHP ?
-The $x (single dollar) is the normal variable with the name x that stores any value like string, integer, float, etc. The $$x (double dollar) is a reference variable that stores the value which can be accessed by using the $ symbol before the $x value. These are called variable variables in PHP.
+PHP Variables Scope:
+In PHP, variables can be declared anywhere in the script.
+The scope of a variable is the part of the script where the variable can be referenced/used.
 
-Variable Variables:- Variable variables are simply variables whose names are dynamically created by another variable’s value.
-
-From the given figure below, it can be easily understood that:
--> $x stores the value "PHP" of String type.
--> Now reference variable $$x stores the value "For PHP"; in it of String type.
+PHP has three different variable scopes:
+-> local
+-> global
+-> static
 */
 
-// Declare variable and initialize it
-$x = "PHP";
-// Reference variable
-$$x = "For PHP";
+//============ 01- Local Scope ==========
+function myLocal() {
+    $x = 5; // local scope
+    echo "Variable x inside function is: $x"."\n";
+  }
+  myLocal();
+  // using x outside the function will generate an error
+  echo "Variable x outside function is: $x"."\n";
+
+
+//============ 02.1- Global not accessed in function ==========
+// A variable declared outside a function has a GLOBAL SCOPE and can only be accessed outside a function:
+
+$x = 5; // global scope
+
+function myGlobal() {
+  // using x inside this function will generate an error: Undefined variable $x in
+  //echo "<p>Variable x inside function is: $x</p>."\n"";
+}
+myGlobal();
+
+echo "<p>Variable x outside function is: $x</p>"."\n";
+
+
+
+
+//============ 02.2- PHP The global Keyword ==========
+/*
+The global keyword is used to access a global variable from within a function.
+To do this, use the global keyword before the variables (inside the function):
+*/
+$x = 5;
+$y = 10;
+
+function globalKeyword() {
+  global $x, $y;
+  $y = $x + $y;
+}
+
+globalKeyword();
+echo $y."\n"; // outputs 15
 
 /*
-So, the value of “for PHP” can be accessed in two ways which are listed below:
--> By using the Reference variable directly. Example: echo $$x;
--> By using the value stored at variable $x as a variable name for accessing the “for PHP” value. Example: echo $PHP; which will give output as “for PHP” (without quote marks).
-Examples:
+PHP also stores all global variables in an array called $GLOBALS[index]. The index holds the name of the variable. This array is also accessible from within functions and can be used to update global variables directly.
+
+The example above can be rewritten like this:
 */
+$a = 30;
+$b = 10;
 
-// Display value of x
-echo $x;    //output = PHP
-echo "\n";
+function globalKeyword2() {
+  $GLOBALS['b'] = $GLOBALS['a'] + $GLOBALS['b'];
+}
 
-// Display value of $$x ($PHP)
-echo $$x;   //output = For PHP
-echo "\n";
-
-// Display value of $PHP
-echo $PHP;  //output = For PHP
-echo "\n";
+globalKeyword2();
+echo $b."\n"; // outputs 15
 
 
-//===================== example 02 ===================== :
-$name = "Tufik";
-$$name = "Bogura";
-echo "$name hasan lives in $Tufik"; //output = Tufik hasan lives in Bogura
-echo "\n";
 
-//===================== example 03 ===================== :
-$age = 25;
-$word = 'age';
-echo $$word;    //output = 25
-echo "\n";
 
-//===================== example 04 ===================== :
-// Declare variable and initialize it
-$var = "PHP";
- 
-// Reference variable
-${$var}="PHPforPHP";
- 
-// Use double reference variable
-${${$var}}="computer science";
- 
-// Display the value of variable
-echo $var . "\n";     //output = PHP
-echo $PHP . "\n";    //output = PHPforPHP
-echo $PHPforPHP . "\n"; //output = computer science
- 
-// Double reference
-echo ${${$var}}. "\n";   //output = computer science
+//============ 03- PHP The static Keyword ==========
+/*
+Normally, when a function is completed/executed, all of its variables are deleted. However, sometimes we want a local variable NOT to be deleted. We need it for a further job.
+
+To do this, use the static keyword when you first declare the variable:
+*/
+function varStaticScope() {
+    static $s = 0;
+    echo $s."\n";
+    $s++;
+  }
+  
+  varStaticScope();
+  varStaticScope();
+  varStaticScope();
