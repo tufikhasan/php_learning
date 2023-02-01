efd3f339ea4fc70041cc220a81962307b008483c
tufik <tufikhasan05@gmail.com>
2023-02-01 15:37:18 +0600

    PHP Data Types

--- index.php
+++ index.php
@@ -1,59 +1,68 @@
 <?php
 /*
-PHP Constants are immutable:
-In PHP, constants are considered immutable, which means that their values cannot be changed once they have been defined. Once a constant is defined using the define() function or the const keyword, its value remains the same throughout the execution of the program. Attempting to change the value of a constant will result in a runtime error.
+PHP Data types:
+Variables can store data of different types, and different data types can do different things.
 
-Constants are typically used to store values that are used throughout a program, such as configuration settings, commonly used values, or other values that are meant to remain constant. By using constants instead of variables, you can ensure that their values remain unchanged, providing a more predictable and secure environment for your code.
+01: Scalar:
+    -> String
+    -> Integer Int
+    -> Float / Double (floating point numbers - also called double)
+    -> Boolean
+02: Compound:
+    -> Array
+    -> Object
+03: Special:
+    -> NULL
+    -> Resource
 
-######### For example: ##########
-define("MY_CONSTANT", "Hello, world!");
-echo MY_CONSTANT; // outputs "Hello, world!"
-
-// Attempting to change the value of a constant will result in an error:
-// MY_CONSTANT = "Goodbye, world!"; // Causes a fatal error
-
-
-********** To create a constant, use the define() ************
-Syntax: define(name, value, case-insensitive)  //Argument #3 is ignored since declaration of case-insensitive constants is no longer supported as of PHP 8.0.0
-
-Parameters:
--> name: Specifies the name of the constant
--> value: Specifies the value of the constant
--> case-insensitive: Defining case-insensitive constants is deprecated as of PHP 7.3.0. As of PHP 8.0.0, only false is an acceptable value, passing true will produce a warning.
 */
 
-//Argument #3 is ignored since declaration of case-insensitive constants is no longer supported as of PHP 8.0.0
-// define("NAME","Tufik Hasan",true);
+//String
+$name = "Tufik";
+var_dump($name);
 
-define("NAME","Tufik Hasan");
-echo NAME;
-echo "\n";
+//Integer Int
+$integer = 2023;
+var_dump($integer);
 
-define("PI",3.14159);
-//show constant value
-echo PI;  //output = 3.14159
-echo "\n";
+//Float / Double
+$floatOrDouble = 10.5;
+var_dump($floatOrDouble);
+
+//Float / Double
+$bool = true;
+var_dump($bool);
+
+//Array
+$arr = array("mango","orange","apple");
+var_dump($arr);
+
+//Object
+class Student{
+    public $name;
+    public $age;
+    public function __construct($name,$age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+    public function message(){
+        return "My name is a $this->name & i am ".$this->age."\n";
+    }
+}
+$student = new Student("Tufik",25);
+echo $student->message();
 
 
-//show constant value
-echo constant("PI");  //output = 3.14159
-echo "\n";
+//Null
+$nullData = null;
+var_dump($nullData);
 
 
-//constants cannot be directly embedded in string literals
-echo "The PI value is PI ";  //output = The PI value is PI
-echo "\n";
+/****** Resources ******
+The special resource type is not an actual data type. It is the storing of a reference to functions and resources external to PHP.
 
-//constants cannot be directly embedded in string literals
-echo "The PI value is {PI}";  //output = The PI value is {PI}
-echo "\n";
+A common example of using the resource data type is a database call.
 
-//Using concatenation:
-echo "The PI value is: ".PI;  //output = The PI value is {PI}: 3.14159
-echo "\n";
-
-
-//tricks to show define constant value in string
-$constant = 'constant';
-echo "The PI value is {$constant("PI")}\n";   //output = The PI value is 3.14159
-echo "The PI value is {$constant("PI")}";   //output = The PI value is 3.14159
+We will not talk about the resource type here, since it is an advanced topic.
+*/
