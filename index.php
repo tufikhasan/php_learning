fd98c2270b8a5042db76ffc83c7f4c51db8ac5db
tufik <tufikhasan05@gmail.com>
2023-02-01 16:20:40 +0600

    PHP some line breaks

--- index.php
+++ index.php
@@ -1,68 +1,22 @@
 <?php
 /*
-PHP Data types:
-Variables can store data of different types, and different data types can do different things.
-
-01: Scalar:
-    -> String
-    -> Integer Int
-    -> Float / Double (floating point numbers - also called double)
-    -> Boolean
-02: Compound:
-    -> Array
-    -> Object
-03: Special:
-    -> NULL
-    -> Resource
-
+PHP line break:
 */
+//01: \r\n -- Newline characters \r\n can be used to create a new line inside the source code.
+echo "Tufik " ."\r\n"."Loves Programming\r\n";
+//02: \n -- Newline characters \n can be used to create a new line inside the source code.
+echo "Tufik " ."\n"."Loves Programming\n";
+//03: PHP_EOL -- representing a line break in a platform-independent way.
+echo "Tufik " .PHP_EOL."Loves Programming".PHP_EOL;
+//04: <br> -- The <br> tag in HTML is used to give the single line break. It is an empty tag, so it does not contain an end tag.
+echo "Tufik ". "<br>". "Loves Programming\n";
 
-//String
-$name = "Tufik";
-var_dump($name);
-
-//Integer Int
-$integer = 2023;
-var_dump($integer);
-
-//Float / Double
-$floatOrDouble = 10.5;
-var_dump($floatOrDouble);
-
-//Float / Double
-$bool = true;
-var_dump($bool);
-
-//Array
-$arr = array("mango","orange","apple");
-var_dump($arr);
-
-//Object
-class Student{
-    public $name;
-    public $age;
-    public function __construct($name,$age)
-    {
-        $this->name = $name;
-        $this->age = $age;
-    }
-    public function message(){
-        return "My name is a $this->name & i am ".$this->age."\n";
-    }
-}
-$student = new Student("Tufik",25);
-echo $student->message();
-
-
-//Null
-$nullData = null;
-var_dump($nullData);
-
-
-/****** Resources ******
-The special resource type is not an actual data type. It is the storing of a reference to functions and resources external to PHP.
-
-A common example of using the resource data type is a database call.
-
-We will not talk about the resource type here, since it is an advanced topic.
+/*
+05: nl2br() -- is a PHP function that converts newline characters (line breaks) to HTML line break elements <br> in a string. It is used to format plain text into HTML-formatted text by preserving line breaks, which would otherwise be ignored by HTML. The nl2br function can be useful for displaying the contents of a textarea form input or the output of a text-based file in an HTML page.
 */
+$text = "This is line 1.\nThis is line 2.";
+echo nl2br($text);
+
+
+
+
