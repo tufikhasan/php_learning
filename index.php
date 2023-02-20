871b2b046b406a98fb27a025dcd4d53a7e662963
tufik <tufikhasan05@gmail.com>
2023-02-20 14:43:49 +0600

    Accessing characters within a string

--- index.php
+++ index.php
@@ -1,37 +1,17 @@
 <?php
-/* Converting to & from ASCII - chr(), ord() */
+/* Accessing characters within a string */
 
-/* -------- ord($string) ------------
-The ord() function is a inbuilt function in PHP that returns the ASCII value of the first character of a string. This function takes a character string as a parameter and returns the ASCII value of the first character of this string.
+$name = "Tufik Hasan";
+echo $name . PHP_EOL;
 
--> https://www.php.net/manual/en/function.ord.php
--> https://www.geeksforgeeks.org/php-ord-function/
--> https://www.w3schools.com/php/func_string_ord.asp
+echo $name[0] . PHP_EOL; //output = T
 
- */
-//Convert the first byte of a string to a value between 0 and 255
-echo ord( "H" ) . PHP_EOL;
-echo ord( "Hello" ) . PHP_EOL;
+echo $name[6] . PHP_EOL; //output = H
 
-/* -------- chr( $asciiVal) ------------
-The chr() function is a built-in function in PHP and is used to convert a ASCII value to a character. It accepts an ASCII value as a parameter and returns a string representing a character from the specified ASCII value. The ASCII value can be specified in decimal, octal, or hex values.
+echo $name[-4] . PHP_EOL; //output = a
 
-# Octal values are defined by a leading 0.
-# Hex values are defined by a leading 0x.
+echo $name[-1] . PHP_EOL; //output = n
 
--> https://www.geeksforgeeks.org/php-chr-function/
--> https://www.php.net/manual/en/function.chr.php
--> https://www.w3schools.com/php/func_string_chr.asp
-
- */
-
-echo chr( "72" ) . PHP_EOL;
-
-//The decimal, octal and hex value of '#' is 35, 043 and 0x23 respectively
-
-//decimal to character
-echo chr( "35" ) . PHP_EOL;
-//octal to character
-echo chr( "35" ) . PHP_EOL;
-//hex to character
-echo chr( "35" ) . PHP_EOL;
+//update string character
+$name[5] = "_";
+echo $name . PHP_EOL;
