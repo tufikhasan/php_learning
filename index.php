4460d7315b9b8d8919e1adf5cd9cc50daed83e96
tufik <tufikhasan05@gmail.com>
2023-02-21 08:59:46 +0600

    trim($string, $characters) - ltrim() - rtrim()

--- index.php
+++ index.php
@@ -1,51 +1,62 @@
 <?php
-/* str_replace($search, $replace, $string, $count)
+/* trim($string, $characters) - ltrim() - rtrim()
  *
-"str_replace()" is a built-in PHP function used to replace all occurrences of a search string with a replacement string within a given string. The function takes three mandatory arguments and an optional fourth argument:
+"trim()" is a built-in function that is used to remove whitespace (or other characters) from the beginning and end of a string.
+
+01: The first parameter "$string" is the input string that you want to trim.
+
+02: The second parameter "$characters" is optional and specifies the characters you want to remove from the string. By default, trim() removes the following characters:
+-> space
+-> tab
+-> newline
+-> carriage return
+-> null character
+-> vertical tab
+
+You can specify any other characters that you want to remove by passing them as a string to the "$characters" parameter.
+
+The "trim()" function returns the trimmed string. Note that the original string is not modified by the function; a new trimmed string is returned.
+
  *
- * https://www.php.net/manual/en/function.str-replace.php
- * https://www.w3schools.com/php/func_string_str_replace.asp
- * https://www.geeksforgeeks.org/php-str_replace-function/
- * https://www.javatpoint.com/php-string-str_replace-function
+ * https://www.php.net/manual/en/function.trim.php
+ * https://www.w3schools.com/php/func_string_trim.asp
+ * https://www.geeksforgeeks.org/php-trim-function/
  */
 
 //01:
-$string = "The quick brown fox jumped over the lazy dog";
-$replace = str_replace( "dog", "cat", $string );
-echo $replace . PHP_EOL;
+$string = "  Tufik Hasan  ";
+echo trim( $string );
 
 //02:
-// $string = "The quick brown fox jumped over the lazy dog";
-// $replace = str_replace( "dog", "cat", $string, $count );
-// echo $replace . PHP_EOL;
-// echo "Total replace : $count\n";
+// $string = "  Tufik \n";
+// echo trim( $string );
+// echo "Hasan";
 
 //03:
-// $string = "The brown brown fox jumped over the lazy dog dog";
-// $replace = str_replace( array( "brown", "dog" ), array( "red", "cat" ), $string, $count );
-// echo $replace . PHP_EOL;
-// echo "Total replace : $count\n";
+// $string = "  Tufik \n";
+// echo trim( $string, " " );
+// echo "Hasan";
 
 //04:
-// $string = "The brown brown fox jumped over the lazy dog dog";
-// $replace = str_replace( ["brown", "dog"], ["red", "cat"], $string, $count );
-// echo $replace . PHP_EOL;
-// echo "Total replace : $count\n";
+// $string = "  Tufik \n";
+// echo trim( $string, "\n" );
+// echo "Hasan";
 
 //05:
-// $string = "fox dog";
-// $replace = str_replace( ["fox", "dog"], "cat", $string, $count );
-// echo $replace . PHP_EOL;
-// echo "Total replace : $count\n";
+// $string = "  Tufik \n";
+// echo trim( $string, " \n" );
+// echo "Hasan";
 
 //06:
-// $string = "fox Fox Dog dog";
-// $replace = str_replace( ["fox", "dog"], "cat", $string, $count );
-// echo $replace . PHP_EOL;
-// echo "Total replace : $count\n";
+// $string = "  .Tufik, \n";
+// // echo trim( $string, " \n," );
+// echo trim( $string, "., \n" );
+// echo "Hasan";
 
-//07: case insensitive
-$string = "fox Fox Dog dog";
-$replace = str_ireplace( ["fox", "dog"], "cat", $string, $count );
-echo $replace . PHP_EOL;
-echo "Total replace : $count\n";
+//08: right trim
+// $string = "  Tufik Hasan  ";
+// echo rtrim( $string );
+
+//07: left trim
+// $string = "  Tufik Hasan  ";
+// echo ltrim( $string );
