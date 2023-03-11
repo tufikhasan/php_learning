fd38cd9422775fa32b43b1135f4907dc5192cd47
tufik <tufikhasan05@gmail.com>
2023-03-11 09:57:29 +0600

    File data processing - Manually

--- README.md
+++ README.md
@@ -320,6 +320,8 @@
 
 05 - [fputcsv() - fgetcsv()](fdf)
 
+06 - [File data processing - Manually](fdf)
+
 # PHP HTML Webpage working with form
 
 01 - [Milligram Mini css framework setup](https://github.com/tufikhasan/php_learning/blob/18b36c1eb55798d00547a2d23f8a48d3d0fc46c5/index.html)
--- index.php
+++ index.php
@@ -1,54 +1,50 @@
 <?php
-/* fputcsv() - fgetcsv() */
+/* File data processing - Manually */
 
-// NOTE: CSV Means - comma separated values
-
-/* --- fputcsv($file, $fields, $separator, $enclosure) ---
-
-The fputcsv() function in PHP is an inbuilt function which is used to format a line as CSV(comma separated values) file and writes it to an open file. The file which has to be read and the fields are sent as parameters to the fputcsv() function and it returns the length of the written string on success or FALSE on failure.
-
- * https://www.php.net/manual/en/function.fputcsv.php
- * https://www.w3schools.com/php/func_filesystem_fputcsv.asp
- * https://www.geeksforgeeks.org/php-fputcsv-function/
- * 
-*/
-//01:
-$filename = "text.txt";
-$fOpen = fopen($filename, 'w');
-fputcsv($fOpen, ['Tufik', 'Hasan', 'Bogura']);
-fputcsv($fOpen, ['Rakib', 'Khan', 'Dhaka']);
+//---------- associative array item write into file ------
+$students = [
+    [
+        "name" => "Tufik",
+        "age" => 25,
+        "roll" => 01,
+        "class" => "BBA",
+    ],
+    [
+        "name" => "Rakib",
+        "age" => 42,
+        "roll" => 17,
+        "class" => "CSE",
+    ],
+    [
+        "name" => "Sabbir",
+        "age" => 32,
+        "roll" => 4,
+        "class" => "MSC",
+    ],
+];
+$fOpen = fopen('students.csv', 'w');
+foreach ($students as $student) {
+    $data = sprintf("%s,%s,%s,%s\n", $student['name'], $student['age'], $student['roll'], $student['class']);
+    fwrite($fOpen, $data);
+}
 fclose($fOpen);
 
-//02:
-$list = array(
-    array("Peter", "Griffin", "Norway"),
-    array("Glenn", "Quagmire", "Germany")
-);
-$file = fopen("contacts.csv", "w");
-foreach ($list as $line) {
-    fputcsv($file, $line);
-}
-fclose($file);
+// //-------- csv file read by line -----------
+// $fOpen = fopen('students.csv', 'r');
+// while ($line = fgets($fOpen)) {
+//     $student = explode(',', $line);
+//     printf("Name = %s\nAge = %s\nRoll = %s\nClass = %s\n\n", $student[0], $student[1], $student[2], $student[3]);
+// }
+// fclose($fOpen);
 
-/* --- fgetcsv($file, $fields, $separator, $enclosure) ---
-
-The fgetcsv() function is a built-in PHP function that reads a single row from a CSV file and returns it as an array of values. It takes one parameter, which is the file handle for the CSV file you want to read from.
-
- * https://www.php.net/manual/en/function.fgetcsv.php
- * https://www.w3schools.com/php/func_filesystem_fgetcsv.asp
- * https://www.geeksforgeeks.org/how-to-parse-a-csv-file-in-php/
- * 
-*/
-//01:
-$filename = "text.txt";
-$fOpen = fopen($filename, 'r');
-$getCsv = fgetcsv($fOpen);
-
-print_r($getCsv);
-fclose($fOpen);
-
-//02:
-$file = fopen("contacts.csv", "r");
-while ($student = fgetcsv($file)) {
-    echo sprintf("Student name is %s %s and lives in %s", $student[0], $student[1], $student[2]) . PHP_EOL;
-}
+// //------------- delete line from file ------------
+// $students = file('students.csv');
+// unset($students[2]);
+// print_r($students);
+// $fOpen = fopen('students.csv', 'w');
+// foreach ($students as $student) {
+//     if ($student != '') {
+//         fwrite($fOpen, $student);
+//     }
+// }
+// fclose($fOpen);
