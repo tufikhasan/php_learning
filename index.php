e844e2c2574c467502c66940c71fbe97ccb317da
tufik <tufikhasan05@gmail.com>
2023-03-14 18:14:30 +0600

    DateTime() Class - Date difference

--- index.php
+++ index.php
@@ -1,76 +1,67 @@
 <?php
-/* strtotime() - Parse about any English textual datetime
-"strtotime()" is a built-in PHP function that converts a human-readable date/time string into a Unix timestamp (i.e., the number of seconds since January 1, 1970, 00:00:00 UTC).
+/* DateTime() Class - Date difference
 
-The "strtotime()" function accepts a string representing the date/time in various formats (such as "now", "tomorrow", "next Monday", "February 15th, 2022", "2022-02-15", etc.), and returns the corresponding Unix timestamp.
+In PHP, the DateTime class is used to represent date and time values. This class allows you to create, manipulate, and format dates and times.
 
-Here are some examples of the possible formats that you can use as the parameter of:
+To create a DateTime object, you can use the new keyword and pass a string representing the date and time as the first argument to the constructor. The format of the string should match the desired output format.
 
-'2022-03-14'
-'2022/03/14'
-'14 Mar 2022'
-'March 14, 2022'
-'2022-03-14 14:30:00 America/New_York'
-'2022-03-14T14:30:00+02:00'
-'2022-03-14 to 2022-03-20'
-
-'14:30:00'
-'2:30pm'
-'14h30m00s'
-'14:30'
-'2pm to 4pm'
-
-'now'
-'today'
-'yesterday'
-'last Month'
-'tomorrow'
-'next Monday'
-'noon'
-'midnight'
-'last Monday to next Friday'
-
-'+1 day'
-'-1 week'
-'+1 month'
-'+1 year'
-'+2 weeks'
-'+2 hours'
-'-30 minutes'
-'first day of +1 month'
-'last day of -1 month'
-'first day of next month'
-
- * https://www.php.net/manual/en/function.strtotime.php
- * https://www.w3schools.com/php/func_date_strtotime.asp
+ * https://www.php.net/manual/en/class.datetime.php
  *
  */
-date_default_timezone_set('Asia/Dhaka');
 
-echo strtotime("now") . "\n";
+//Date difference using strtotime() function
+$date1 = strtotime('1 jan 2023 12pm');
+$date2 = strtotime('31 january 2023 12AM');
+$defference = $date2 - $date1;
+echo ($date2 - $date1) / (86400) . PHP_EOL;
 
-echo strtotime("last Month") . "\n";
 
-echo strtotime("next month") . "\n";
+//01: DateTime() - Date difference
+$date1 = new DateTime('1 jan 2022 3pm');
+$date2 = new DateTime('31 jan 2023 3pm');
 
-echo (strtotime("+1 week -2 days +72 hours") - strtotime("now")) / (86400), "\n";
+$diff = $date1->diff($date2);
+echo $diff->d . PHP_EOL;
 
-echo date('jS-F-Y h:i:s a', strtotime('+3 days 2hours 5 minutes 23 seconds')) . "\n";
 
-echo date('dS/M/Y h:i:s a', strtotime("midnight")) . "\n";
+//02: DateTime() - Date difference
+$date1 = new DateTime('1 jan 2023 3pm');
+$date2 = new DateTime('31 jan 2023 3pm');
 
-echo date('h:i:s a', strtotime("noon")) . "\n";
+$diff = $date1->diff($date2);
+echo $diff->y > 0 ? $diff->y . PHP_EOL : "Zero\n";
 
-echo date('h:i:s a', strtotime("today 8pm")) . "\n";
 
-echo date('dS.M.Y h:i:s a', strtotime("first day of -1 month")) . "\n";
+//03: DateTime() - Date difference
+$date1 = new DateTime('1 jan 2022');
+$date2 = new DateTime('25 july 2023');
 
-echo date('dS.M.Y', strtotime("first friday dec 2022")) . "\n";
+$diff = $date1->diff($date2);
+echo $diff->d . " day " . $diff->m . " month " . $diff->y . " year " . PHP_EOL;
 
-echo date('dS.M.Y', strtotime(" +7 day")) . "\n";
 
-echo date('D-dS.M.Y h:i:s a', strtotime("12/5/1997")) . "\n";
+//04: DateTime() - Date difference
+$date1 = new DateTime('1 jan 2022');
+$date2 = new DateTime('25 july 2023');
 
-echo date('dS.M.Y', strtotime("next friday end month")) . "\n";
+$diff = $date1->diff($date2);
+echo isPluralOrSingle($diff->d, "day") . isPluralOrSingle($diff->m, "month") . isPluralOrSingle($diff->y, "year") . PHP_EOL;
 
-echo date('D dS.M.Y h:i:s a', strtotime("now America/New_York")) . "\n";
+
+function isPluralOrSingle($number, $type)
+{
+    if ($number == 0) {
+        return "";
+    } elseif ($number > 1) {
+        return "{$number} {$type}s ";
+    } else {
+        return "{$number} {$type} ";
+    }
+}
+
+
+//----------
+$date = new DateTime('2023-03-14 12:00:00');
+$date->add(new DateInterval('P1D')); //1day plus
+$formatted_date = $date->format('Y-m-d H:i:s');
+echo $formatted_date;
