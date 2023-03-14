8175e3994f17d52fcb90eaa62ee5a6f74dc6182d
tufik <tufikhasan05@gmail.com>
2023-03-14 22:09:07 +0600

    date_create(), date_add(), date_sub(), date_format()

--- README.md
+++ README.md
@@ -308,6 +308,8 @@
 
 09 - [date_diff() - Date difference](GF)
 
+10 - [date_create(), date_add(), date_sub(), date_format()](GF)
+
 # PHP directory and file-system
 
 01 - [PHP Some commonly used directory and file functions](https://github.com/tufikhasan/php_learning/blob/7f40c7e7452ca00f0693d899d007bd51b337d510/index.php)
--- index.php
+++ index.php
@@ -1,41 +1,27 @@
 <?php
-/* date_diff() - Date difference
+/* date_create(), date_add(), date_sub(), date_format()
 
- * https://www.geeksforgeeks.org/php-date_diff-function/
- * https://www.php.net/manual/en/datetime.diff.php
- * https://www.w3schools.com/php/func_date_date_diff.asp
+-> date_create() - create a new DateTime object
+-> date_add() - Alias of DateTime::add()
+-> date_sub() - Alias of DateTime::sub()
+-> date_format() - Alias of DateTime::format()
+-> date_interval_create_from_date_string() — Alias of DateInterval::createFromDateString()
+
+ * https://www.php.net/manual/en/ref.datetime.php
+ * https://www.w3schools.com/php/php_ref_date.asp
  *
  */
 
-$date1 = new DateTime( '1 jan 2023' );
-$date2 = new DateTime( '31 january 2023' );
+// date_create()
+$date = date_create( '2019-01-01' );
+echo date_format( $date, 'Y-M-d' ) . PHP_EOL;
 
-//01:
-$diff = date_diff( $date1, $date2 );
-echo $diff->format( "%d" ) . PHP_EOL;
+// date_add()
+$date = date_create( '2019-01-01' );
+date_add( $date, date_interval_create_from_date_string( '1 year 30 days' ) );
+echo date_format( $date, 'Y-M-d' ) . PHP_EOL;
 
-//02:
-$diff = date_diff( $date1, $date2 );
-echo $diff->d . PHP_EOL;
-
-//03:
-$diff = date_diff( $date1, $date2 );
-
-echo isPluralOrSingle( $diff->d, "day" ) . isPluralOrSingle( $diff->m, "month" ) . isPluralOrSingle( $diff->y, "year" ) . PHP_EOL;
-
-function isPluralOrSingle( $number, $type ) {
-    if ( $number == 0 ) {
-        return "";
-    } elseif ( $number > 1 ) {
-        return "{$number} {$type}s ";
-    } else {
-        return "{$number} {$type} ";
-    }
-}
-
-//04:
-$date1 = date_create( '1 jan 2023' );
-$date2 = date_create( '31 january 2023' );
-
-$diff = date_diff( $date1, $date2 );
-echo $diff->d . PHP_EOL;
+// date_sub()
+$date = date_create( "2019-01-01" );
+date_sub( $date, date_interval_create_from_date_string( "1 year 30 days" ) );
+echo date_format( $date, "Y-M-d" );
